7d213c6ef46707460cf12361eccaf96386602889
Julian Diaz <jdiaz@e1z2r5p1.42.us.org>
2018-12-04 21:57:24 -0800

    0

--- assembler/srcs/assembler.c
+++ assembler/srcs/assembler.c
@@ -6,21 +6,34 @@
 /*   By: jdiaz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 19:14:37 by jdiaz             #+#    #+#             */
-/*   Updated: 2018/12/04 21:15:28 by jdiaz            ###   ########.fr       */
+/*   Updated: 2018/12/04 21:57:02 by jdiaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <assembler.h>
 
-int		main(int argc, char **argv)
+int		lexer(char *str)
 {
 	int fd;
 
+	fd = open(str, O_RDONLY);
+}
+
+int		main(int argc, char **argv)
+{
+	t_asm	ob;
+	int		fd;
+
 	if (argc != 2)
-		printf("arrgument must be a file\n");
+		printf("incorrect number of arguments\n");
 	else
 	{
-		
+		if (lexer(argv[1]) == -1)
+			return(printf("error in the input file\n"));
+		ob.labels = get_labels(&ob);
+		fd = open("new_file", O_WRONLY);
+		write_bits(&ob, fd);
+		close(fd);
 	}
 	return (0);
 }
