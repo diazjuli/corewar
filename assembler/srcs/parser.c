def506a692d46f2beefd4a51eaf119a572e70c7c
Julian Diaz <jdiaz@e1z3r3p16.42.us.org>
2018-12-06 19:59:19 -0800

    finished second pass for assembler, need to test for bugs

--- assembler/srcs/parser.c
+++ assembler/srcs/parser.c
@@ -6,26 +6,63 @@
 /*   By: jcruz-y- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 18:29:05 by jcruz-y-          #+#    #+#             */
-/*   Updated: 2018/12/05 21:04:32 by jdiaz            ###   ########.fr       */
+/*   Updated: 2018/12/06 19:58:19 by jdiaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "assembler.h"
 
-int		get_label_address(char *filename, t_vars *ob)  //2nd pass, while counting get label addresses and check lex-labels
+int		count_params(int op_code, int num_args, int *arg_types, char *args)
 {
-	int		fd;
-	int		counter;
-	char	**line;
+	int	mem;
+	int i;
+	char **split;
 
-	fd = open(filename, O_RDONLY);
-	counter = 0;
-	while ((get_next_line(fd, line) > 0))
+	i = 0;
+	mem = 0;
+	split = ft_strsplit(args, SEPARATOR_CHAR);
+	while (i < num_args)
 	{
-		get_label_addr(line, ob);
-		check_label(line, ob);   //check the label is correct within the arg, get the label value inside the struct
+		if (split[i][0] == 'r')
+			mem++;
+		else if (split[i][0] == DIRECT_CHAR)
+			mem += DIR_SIZE;
+		else
+			mem += IND_SIZE;
+	}
+	return (mem);
+}
 
+/*
+** Second pass through the file. Will check validity of all labels used and 
+** get addresses for each label. Uses counter to keep track of current address
+*/
 
+int		get_label_address(t_vars *ob, int fd)
+{
+	int		counter;
+	char	*line;
+	char	**inst;
+
+	counter = 0;
+	while ((get_next_line(fd, &line) > 0))
+	{
+		ob->label = 0;
+		if (line[0] != COMMENT_CHAR && line[0] != '.' && ft_strcmp(line, "\n"))
+		{
+			inst = ft_strsplit(line, ' ');
+			if (ft_strchr(inst[0], LABEL_CHAR))
+			{
+				set_address(inst[0], counter);
+				ob->label = 1;
+			}
+			ob->op_code = get_op(inst[ob->label]);
+			counter += 1 + op_tab[ob->op_code].encoding_byte;
+			count += count_params(ob->op_code, op_tab[ob->op_code].num_args,
+					op_tab[ob->op_code].arg_types, inst[ob->label + 1]);
+			free_split(inst);
+		}
+		free(line);
 	}
 	return (1);
 }
