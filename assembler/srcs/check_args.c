97b9c900f0aa9f6a2242327796c0168b2494a940
Jose miguel Cruz y celis <jcruz-y-@e1z2r3p5.42.us.org>
2018-12-10 15:37:05 -0800

    added macros to variables (as in config type practices)

--- assembler/srcs/check_args.c
+++ assembler/srcs/check_args.c
@@ -6,7 +6,7 @@
 /*   By: jcruz-y- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 18:28:52 by jcruz-y-          #+#    #+#             */
-/*   Updated: 2018/12/10 13:38:00 by jcruz-y-         ###   ########.fr       */
+/*   Updated: 2018/12/10 15:22:33 by jcruz-y-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		check_register(char *reg)
 	i = 1;
 	while (reg[i])
 	{
-		if (reg[i] >= '0' && reg[i] <= '9' || reg[ft_strlen(reg) - 1] == ',')
+		if (reg[i] >= '0' && reg[i] <= '9' || reg[ft_strlen(reg) - 1] == SEPARATOR_CHAR)
 			i++;
 		else
 		{
@@ -29,7 +29,7 @@ int		check_register(char *reg)
 		}
 	}
 	reg_num = ft_atoi(reg+1);
-	if (reg_num > 16)
+	if (reg_num > REG_NUMBER)
 	{
 		printf("register number too high\n");
 		exit (0);
@@ -42,15 +42,16 @@ int		check_direct(char *direct)   //can be %:alphanumeric or %numeric
 	int	i;
 
 	i = 1;
-	if (direct[1] == ':') //must be a valid label, but we don hav no label list
+	if (direct[1] == LABEL_CHAR) //must be a valid label, but we don hav no label list
 	{
 		//check label?
+		//continue?
 	}
 	else
 	{
-		while (direct[i]) //not managing comma in between the word
+		while (direct[i]) 
 		{
-			if (direct[i] >= '0' && direct[i] <= '9' || direct[ft_strlen(direct) - 1] == ',')
+			if (direct[i] >= '0' && direct[i] <= '9' || direct[ft_strlen(direct) - 1] == SEPARATOR_CHAR)
 				i++;
 			else
 			{
@@ -70,7 +71,7 @@ int	check_indirect(char *ind)
 	i = 0;
 	while (ind[i])
 	{
-		if (ind[i] >= '0' && ind[i] <= '9' || ind[ft_strlen(ind) - 1] == ',')
+		if (ind[i] >= '0' && ind[i] <= '9' || ind[ft_strlen(ind) - 1] == SEPARATOR_CHAR)
 			i++;
 		else
 		{
@@ -89,13 +90,13 @@ int		check_args(int num_args, int *arg_types, char **inst, t_vars *ob)
 	i = ob->bl_label + 2;
 	while (i < num_args)
 	{
-		if (inst[i][0] == 'r' && (T_REG | arg_types[i] == T_REG))
+		if (inst[i][0] == 'r' && (T_REG & arg_types[i] == T_REG))
 		{
 			check_register(inst[i]);
 		}
-		else if (inst[i][0] == '%' && (T_DIR | arg_types[i] == T_DIR)
+		else if (inst[i][0] == DIRECT_CHAR && (T_DIR & arg_types[i] == T_DIR)
 			check_direct(inst[i]);
-		else if (inst[i][0] >= '0' && inst[i][0] <= '9' && (T_IND | arg_types[i] == T_IND)
+		else if (inst[i][0] >= '0' && inst[i][0] <= '9' && (T_IND & arg_types[i] == T_IND)
 			check_indirect(inst[i]);
 		i++;
 	}
