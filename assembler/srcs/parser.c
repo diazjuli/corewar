abcf3d9cb6c50f4651ede3ec03a86cf4f02cb165
Julian Diaz <jdiaz@e1z2r5p27.42.us.org>
2018-12-11 17:22:47 -0800

    changed strsplit

--- assembler/srcs/parser.c
+++ assembler/srcs/parser.c
@@ -6,26 +6,24 @@
 /*   By: jcruz-y- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 18:29:05 by jcruz-y-          #+#    #+#             */
-/*   Updated: 2018/12/10 21:19:29 by jdiaz            ###   ########.fr       */
+/*   Updated: 2018/12/11 17:01:51 by jdiaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "assembler.h"
 
-int		count_params(int op_code, int num_args, char *args)
+int		count_params(int op_code, int num_args, char **inst, int bl)
 {
 	int	mem;
 	int i;
-	char **split;
 
 	i = 0;
 	mem = 0;
-	split = ft_strsplit(args, SEPARATOR_CHAR);
 	while (i < num_args)
 	{
-		if (split[i][0] == 'r')
+		if (inst[i + 1 + bl][0] == 'r')
 			mem++;
-		else if (split[i][0] == DIRECT_CHAR && check_index(op_code) != 1)
+		else if (inst[i + 1 + bl][0] == DIRECT_CHAR && check_index(op_code) != 1)
 			mem += DIR_SIZE;
 		else
 			mem += IND_SIZE;
@@ -64,10 +62,10 @@ int		get_label_address(t_vars *ob, int fd)
 	counter = 2176;
 	while ((get_next_line(fd, &line) > 0))
 	{
-		ob->labels = 0;
+		ob->bl_label = 0;
 		if (line[0] != COMMENT_CHAR && line[0] != '.' && ft_strcmp(line, "\n"))
 		{
-			inst = ft_strsplit(line, ' ');
+			inst = ft_strsplit(line, " ,");
 			if (ft_strchr(inst[0], LABEL_CHAR))
 			{
 				set_address(inst[0], counter, ob);
@@ -76,7 +74,7 @@ int		get_label_address(t_vars *ob, int fd)
 			ob->op_code = get_op(inst[ob->bl_label]);
 			counter += 1 + op_tab[ob->op_code].encoding_byte;
 			counter += count_params(ob->op_code, op_tab[ob->op_code].num_args,
-					inst[ob->bl_label + 1]);
+					inst, ob->bl_label);
 			free_split(inst);
 		}
 		free(line);
