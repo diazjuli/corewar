bc3cf93ee9f71934bf45a30d8b2a267169e6ac08
Julian Diaz <jdiaz@e1z2r5p31.42.us.org>
2018-12-12 21:59:06 -0800

    fixed issue with counter

--- assembler/srcs/generator.c
+++ assembler/srcs/generator.c
@@ -6,7 +6,7 @@
 /*   By: jcruz-y- <jcruz-y-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 18:28:57 by jcruz-y-          #+#    #+#             */
-/*   Updated: 2018/12/12 18:32:13 by jdiaz            ###   ########.fr       */
+/*   Updated: 2018/12/12 21:52:02 by jdiaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,8 @@ int		print_params(t_vars *ob, char **inst, int op_code, int begin_address)
 
 	i = ob->bl_label + 1;
 	counter = 0;
-	while (inst[i] != NULL)
+	while (inst[i] != NULL && counter != -1)
 	{
-		
 		if (inst[i][0] == 'r')
 		{
 			counter++;
@@ -50,13 +49,13 @@ int		print_params(t_vars *ob, char **inst, int op_code, int begin_address)
 		}
 		else if (inst[i][0] == DIRECT_CHAR && check_index(op_code) == -1)
 		{
-			counter += 4;
-			print_direct(inst[i], ob, begin_address);
+			counter = print_direct(inst[i], ob, begin_address) == -1 ? -1
+				: counter + 4;
 		}
 		else
 		{
-			counter += 2;
-			print_indirect(inst[i], ob, begin_address);
+			counter = print_indirect(inst[i], ob, begin_address) == -1 ?
+				-1 : counter + 2;
 		}
 		i++;
 	}
@@ -96,6 +95,7 @@ int		print_encoding(t_vars *ob, int op_code, char **inst, int num_params)
 int		print_inst(t_vars *ob, char **inst, int counter)
 {
 	int		begin_address;
+	int		temp;
 
 	begin_address = counter;
 	if (ft_strchr(inst[0], LABEL_CHAR))
@@ -111,7 +111,8 @@ int		print_inst(t_vars *ob, char **inst, int counter)
 	counter++;
 	counter += print_encoding(ob, ob->op_code, inst,
 			op_tab[ob->op_code].num_args);
-	counter += print_params(ob, inst, ob->op_code, begin_address);
+	temp = print_params(ob, inst, ob->op_code, begin_address);
+	counter = temp == -1 ? -1 : counter + temp; 
 	free_split(inst);
 	return (counter);
 }
@@ -144,7 +145,7 @@ int		generator(t_vars *ob, int fd)
 				if ((counter = print_inst(ob, inst, counter)) == -1)
 					return (-1);
 			}
-		}	
+		}
 		free(line);
 	}
 	return (1);
